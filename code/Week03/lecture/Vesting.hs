00f091e6f71468738000d7f78529e771d0444925
Roberto <vortecsmaster@vortecsla.com>
2023-03-21 12:53:17 -0500

    Contrato Simple para el TBS demo
    contrato simple para el TBS demo

--- code/Week03/lecture/Vesting.hs
+++ code/Week03/lecture/Vesting.hs
@@ -15,7 +15,7 @@ import           Plutus.V2.Ledger.Contexts (txSignedBy)
 import           PlutusTx                  (compile, unstableMakeIsData)
 import           PlutusTx.Prelude          (Bool, traceIfFalse, ($), (&&))
 import           Prelude                   (IO, String)
-import           Utilities                 (Network, posixTimeFromIso8601,
+)import           Utilities                 (Network, posixTimeFromIso8601,
                                             printDataToJSON,
                                             validatorAddressBech32, wrap,
                                             writeValidatorToFile)
@@ -44,18 +44,31 @@ mkVestingValidator dat () ctx = traceIfFalse "beneficiary's signature missing" s
     deadlineReached :: Bool
     deadlineReached = contains (from $ deadline dat) $ txInfoValidRange info
 
+{-# INLINABLE tbsTest #-}
+tbsTest :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+tbsTest datum  redeemer _ 
+ | datum == redeemer   = ()
+ | otherwise           = error ()
+
 {-# INLINABLE  mkWrappedVestingValidator #-}
 mkWrappedVestingValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkWrappedVestingValidator = wrap mkVestingValidator
 
+
+
 validator :: Validator
 validator = mkValidatorScript $$(compile [|| mkWrappedVestingValidator ||])
 
+tbsValidator :: Validator
+tbsValidator :: mkValidatorScript $$(compile [|| tbsTest ||])
 ---------------------------------------------------------------------------------------------------
 ------------------------------------- HELPER FUNCTIONS --------------------------------------------
 
 saveVal :: IO ()
 saveVal = writeValidatorToFile "./assets/vesting.plutus" validator
+
+creaTBSplutus :: IO ()
+creaTBSplutus =  writeValidatorToFile "./testnet/tbsTest.plutus" validator
 
 vestingAddressBech32 :: Network -> String
 vestingAddressBech32 network = validatorAddressBech32 network validator
